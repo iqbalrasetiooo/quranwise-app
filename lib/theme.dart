fc61b2703e3251755d5c061c9581f47b9b578ff0
Iqbal Rasetio <rasetioiqbal@gmail.com>
2023-01-05 19:16:20 +0700

    add theme.dart

---
+++ lib/theme.dart
@@ -0,0 +1,6 @@
+import 'export.dart';
+
+const Color primaryColor = Color(0xff6FC6AA);
+const Color whiteColor = Color(0xffFFFFFF);
+const Color blackColor = Color(0xff1F2127);
+const Color backgroundColor = Color(0xffF9F9F9);
